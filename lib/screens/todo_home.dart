9ab9c7b37087a5d146cdce45cb127fafb05321a3
T-jay A. Abunales <tjabunales@gmail.com>
2024-09-14 00:55:33 +0800

    integrated TaskField widget with TodoHome for adding tasks

--- lib/screens/todo_home.dart
+++ lib/screens/todo_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/task.dart';
+import '../widgets/add_task_field.dart';
 
 class TodoHome extends StatefulWidget {
   @override
@@ -6,14 +8,44 @@ class TodoHome extends StatefulWidget {
 }
 
 class _TodoHomeState extends State<TodoHome> {
+  final List<Task> _tasks = [];
+  final TextEditingController _taskController = TextEditingController();
+
+  void _addTask() {
+    if (_taskController.text.isEmpty) return;
+    setState(() {
+      _tasks.add(Task(id: DateTime.now().toString(), title: _taskController.text));
+    });
+    _taskController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('To-Do List'),
       ),
-      body: Center(
-        child: Text('To-Do List Home'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: AddTaskField(
+              taskController: _taskController,
+              onAddTask: _addTask,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final task = _tasks[index];
+                return ListTile(
+                  title: Text(task.title),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
